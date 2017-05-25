9605982e767b467de3abcd7d8a53fa3ac808d465
Liang Cheng <liangcheng6@gmail.com>
2017-05-25 16:07:36 -0400

    more trying

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,12 @@
-modeule.exports = function(aa0) {
+var fridges_route = require("./controllers/fridges_contoller");
+
+module.exports = function(app) {
     //map some api to these function
     // app.get("/api/all", function(req, res) {
     //    do something
     //
     // });
+    app.use("/fridges", fridges_route);
+    app.use("/fridges/update", fridges_route);
+    app.use("/fridges/create", fridges_route);
 };
---
+++ routes/handlebars-routes.js
@@ -0,0 +1,7 @@
+modeule.exports = function(app) {
+    //map some api to these function
+    // app.get("/api/all", function(req, res) {
+    //    do something
+    //
+    // });
+};
