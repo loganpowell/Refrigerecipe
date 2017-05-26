5cb2d005f078d4ffe79df326b51a687d3c0451bc
Liang Cheng <liangcheng6@gmail.com>
2017-05-25 20:36:56 -0400

    misc. updates

--- controllers/fridges_controller.js
+++ controllers/fridges_controller.js
@@ -17,14 +17,20 @@ module.exports = function (app) {
             });
     });
 
+    app.get("/fridges/:id", function (req, res) {
+        fridge_contents.findAll({fridge_id:1})
+            .then(function(data) {
+                // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+                var hbsObject = { fridges: data };
+                //res.render("fridges", hbsObject);
+                res.render("fridges", hbsObject);
+            });
+    });
 
-// put route -> back to index
     app.put("/fridges/update", function(req, res) {
-        fridges.update(req.body.burger_id, function(result) {
-            // wrapper for orm.js that using MySQL update callback will return a log to console,
-            // render back to index with handle
-            console.log(result);
-            res.redirect("/");
-        });
+        // fridge_contents.update(req.body.burger_id, function(result) {
+        //     console.log(result);
+        //     res.redirect("/");
+        // });
     })
 }
