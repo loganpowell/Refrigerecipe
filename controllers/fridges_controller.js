e824b48f4fbe7d06fa6e43e5c8825a46f19371fc
Liang Cheng <liangcheng6@gmail.com>
2017-05-27 16:16:15 -0400

    first update of fridges API

--- controllers/fridges_controller.js
+++ controllers/fridges_controller.js
@@ -2,38 +2,105 @@
 
 var express = require("express");
 var db = require("../models");
+var _ = require('underscore')
 var fridges = db.fridges;
 var fridge_contents = db.fridge_contents;
 
 
+
 module.exports = function (app) {
-    app.get("/fridges", function(req, res) {
-        // express callback response by calling burger.selectAllBurger
-        fridge_contents.findAll({fridge_id:1})
-            .then(function(data) {
-                // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-                var hbsObject = { fridges: data };
-                //res.render("fridges", hbsObject);
-                res.render("fridges", hbsObject);
-            });
-    });
+  //**** start of view renderers ****/
+  app.get("/fridges", function (req, res) {
+    // express callback response by calling burger.selectAllBurger
+    fridge_contents.findAll({fridge_id: 1})
+      .then(function (data) {
+        // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+        var hbsObject = {fridges: data};
+        //res.render("fridges", hbsObject);
+        res.render("fridges", hbsObject);
+      });
+  });
 
-    //todo: get the id to the internal
-    app.get("/fridges/:id", function (req, res) {
-        fridge_contents.findAll({fridge_id:1})
-            .then(function(data) {
-                // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-                var hbsObject = { fridges: data };
-                //res.render("fridges", hbsObject);
-                res.render("fridges", hbsObject);
-            });
-    });
 
-    //todo: what do I need to do update?
-    app.put("/fridges/update", function(req, res) {
-        // fridge_contents.update(req.body.burger_id, function(result) {
-        //     console.log(result);
-        //     res.redirect("/");
-        // });
-    })
+  /***** end of view renderers ****/
+
+  /****** start of API end points ****/
+
+  app.get("/fridges/:id", function (req, res) {
+
+    var fridgeId = parseInt(req.params.id, 10);
+    fridges.findById(fridgeId)
+      .then(function (data) {
+        // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+        if (!!data) {
+          var resData = {id: data.id, fridge_name :data.fridge_name, user_id: data.user_id};
+          fridge_contents.findAll({where: {fridge_id:fridgeId}})
+            .then(function(data) {
+              if(!!data) {
+                var fridge_contents = data.map(function(elem) {
+                  return {id:elem.id, ingredient:elem.ingredient, servings_count:elem.ingredient_count};
+                });
+                resData['fridge_contents'] =fridge_contents;
+                res.json(resData);
+              }else {
+                //there's no content in fridge, directly return resData;
+                resData['fridge_contents'] = [];
+                res.json(resData);
+              }
+            }, function(err) {
+              res.status(500).send(err);
+            });
+
+        }else {
+          res.status(404).send();
+        }
+      }, function(err) {
+        res.status(500).send(err);
+      });
+  });
+
+  //todo: create a new fridge
+  app.post("/fridges/create", function (req, res) {
+    //var user_id = parseInt(req.body, 'user_id', 'fridge_name');
+    var body = _.pick(req.body, 'user_id', 'fridge_name');
+
+    fridges.create(body)
+      .then(function (fridge) {
+        res.json(fridge.toJSON());
+      }, function (err) {
+        res.status(400).json(err);
+      });
+
+  });
+
+  //PUT /fridges/:id
+  app.put("/fridges/:id", function(req, res) {
+    //todo change this
+    var user_id = 1;
+    var fridge_id = parseInt(req.params.id, 10);
+    var fridge_contents = _.pick(req.body, 'fridge_contents');
+
+    fridges.findById(fridge_id)
+      .then(function(fridge) {
+        if(fridge) {
+          //todo: update fridge_contents in here
+          //
+
+        }else {
+          res.status(404).send();
+        }
+      },function(err)  {
+        res.status(500).send();
+      })
+
+  });
+  //todo: what do I need to do update?
+  app.put("/fridges/update", function (req, res) {
+    // fridge_contents.update(req.body.burger_id, function(result) {
+    //     console.log(result);
+    //     res.redirect("/");
+    // });
+  });
+
+  /****** end of API end points *******/
 };
