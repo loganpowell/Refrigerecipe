b14bb571c3442fae121a1394e2f70db2500774bc
Liang Cheng <liangcheng6@gmail.com>
2017-05-29 16:46:55 -0400

    initial work to integrate spoonacular api

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -1,9 +1,21 @@
 "use strict"
 var express = require("express");
+var unirest = require('unirest');
 
 module.exports = function (app) {
     app.get("/recipes", function(req, res) {
         // express callback response by calling burger.selectAllBurger
         res.render("recipes", {});
     });
-}
+
+    app.get("recipes/:id", function(req, res) {
+      //todo: replace the ingredient data with real data
+      //todo: replace the other parametes with real data.
+      var ingredients = ["Egg", "Chicken"];
+      var fridge_id = parseInt(req.params.id, 10);
+      var user_id = 1;
+
+
+
+    })
+};
