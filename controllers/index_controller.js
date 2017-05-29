0ccfeb321635774afec00d8623bd5130ffb1461c
Liang Cheng <liangcheng6@gmail.com>
2017-05-29 17:22:05 -0400

    remove on useless comment.

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -5,7 +5,6 @@ module.exports = function (app) {
   //**** start of view renderers ****/
   app.get("/", function (req, res) {
     //todo rewite this
-    // express callback response by calling burger.selectAllBurger
     res.render("index");
   });
 };
