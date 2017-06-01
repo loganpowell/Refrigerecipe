92937deaf08502c10f3e1b6951369014c36e44c4
Liang Cheng <liangcheng6@gmail.com>
2017-06-01 00:18:03 -0400

    intermediate step of inserting ingredient into ingredients UI

--- public/js/FridgeUI.js
+++ public/js/FridgeUI.js
@@ -1,8 +1,9 @@
 function FridgeUI() {
-  var ingredients;
-  var fridge_name;
-  var user_id;
-  var id;
+  this.ingredients = [];
+  this.fridge_name = "";
+  this.user_id = "";
+  //todo change this thing
+  this.id = 1;
 }
 //example output from ajax query
 // Object {id: 1, fridge_name: "Happy Fridge", user_id: 1, ingredients: Array(5)}
@@ -22,39 +23,63 @@ function FridgeUI() {
 //this function takes the resposne from JSON api /fridge/:id
 // and feed it to the next function.
 FridgeUI.prototype.getFridgeIngredients = function () {
+  var fridgeId = this.id;
   return new Promise (
     function(resolve, reject) {
       //todo: resolve this id issue.
       var this_id = this.id;
-      console.log("Fridge id: " + this_id);
+      console.log("Fridge id: " + fridgeId);
 
       //todo: modify the fridge ID
-      var queryURL = "/fridge/1";
+      var queryURL = "/fridge/" + fridgeId;
       $.ajax( {
         url:queryURL,
         method:"GET"
       })
         .done(function(response) {
-          resolve(response);
-        })
+          var ingredient_data = _.pick(response, 'ingredients');
+          this.ingredients = ingredient_data["ingredients"];
+          resolve();
+        }.bind(this))
         .fail(function(err) {
           reject(err);
         });
-    }
+    }.bind(this)
+    //use bind this to get make sure all (this) is the FridgeUI object.  Otherwise, this goes all over the place
   );
 };
 
-//this function will take the api response data and feed it to
-// the handlebars displya
-FridgeUI.prototype.displayFridgeIngredients = function(resp) {
-  console.log(resp);
-  var ingredient_data = _.pick(resp, 'ingredients');
-  console.log(ingredient_data);
+//this function will display FridgeUI object's ingredient data.
+FridgeUI.prototype.displayFridgeIngredients = function() {
+  //console.log(resp);
+  //var ingredient_data = _.pick(resp, 'ingredients');
+  //this.ingredients = ingredient_data["ingredients"];
+  console.log(this.ingredients);
+  //console.log(ingredient_data);
   var source = $("#fridge-ingredient-template").html();
   var template =Handlebars.compile(source);
-  var html = template(ingredient_data);
+  var html = template({"ingredients": this.ingredients});
   console.log(html);
   $("#ingredient-list-div").append(html);
+};
+
+FridgeUI.prototype.clearFridgeIngredients = function(resp) {
+  $("#ingredient-list-div").empty();
+};
+
+FridgeUI.NEW_INGREDIENT_ENTRY_ID = '#new_ingredient_entry';
+FridgeUI.NEW_INGREDIENT_CHECK_BTN_ID = '#new_ingredient_btn';
+
+FridgeUI.prototype.addNewIngredientBtnClick = function() {
+  this.clearFridgeIngredients();
+
+  var ingredient_name = $(FridgeUI.NEW_INGREDIENT_ENTRY_ID).val();
+  var newIngredient = {"ingredient": ingredient_name, "servings_count":1};
+  this.ingredients.push(newIngredient);
+  console.log(ingredient_name);
+
+  this.displayFridgeIngredients();
+  this.addIngredientOnOffButton();
 
 };
 
@@ -79,10 +104,11 @@ FridgeUI.prototype.putFridgeIngredients = function(id, data) {
 
 window.onload = function() {
   window.fridgeUI = new FridgeUI();
-  window.fridgeUI.id = 1;
+
+  $(FridgeUI.NEW_INGREDIENT_CHECK_BTN_ID).click(window.fridgeUI.addNewIngredientBtnClick.bind(window.fridgeUI));
   //window.fridgeUI.getFridgeIngredients(1);
   window.fridgeUI.getFridgeIngredients()
-    .then(window.fridgeUI.displayFridgeIngredients)
+    .then(window.fridgeUI.displayFridgeIngredients.bind(window.fridgeUI))
     .then(window.fridgeUI.addIngredientOnOffButton);
   //templatingTest();
 };
