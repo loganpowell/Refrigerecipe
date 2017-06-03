142ead08b51b56187c9e5a1053d94ae087311839
Liang Cheng <liangcheng6@gmail.com>
2017-06-03 04:35:42 -0400

    add additional modules, and update auth0 account to Liang’s

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 var methodOverride = require("method-override");
 var db = require("./models");
 
