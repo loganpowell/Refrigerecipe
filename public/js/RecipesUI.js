a0ba89980f5f876a4d345e3a5542a9542e37947a
Liang Cheng <liangcheng6@gmail.com>
2017-06-02 21:47:29 -0400

    intermediate checkin that contains get of recipes detail.

--- public/js/RecipesUI.js
+++ public/js/RecipesUI.js
@@ -1,5 +1,8 @@
+"use strict";
+
 function RecipesUI() {
-  var recipesList = [];
+  this.recipes = [];
+  this.recipes_details = {};
 }
 
 RecipesUI.prototype.getAllRecipes = function() {
@@ -26,15 +29,46 @@ RecipesUI.prototype.getAllRecipes = function() {
 
 RecipesUI.prototype.displayAllRecipes = function(resp) {
   console.log(resp);
-  var recipes = {"recipes": resp};
+  this.recipes = resp;
+
   var source = $("#recipe-card-template").html();
   var template =Handlebars.compile(source);
-  var html = template(recipes);
-  console.log(html);
+  var html = template({"recipes": resp});
+  //console.log(html);
   $("#recipes-list-div").append(html);
   $(".popper").click(function(){
     $(this).siblings(".ui.modal.test").modal('show');
   });
+  return resp
+};
+
+RecipesUI.prototype.setupRecipesModalCards = function() {
+
+  var recipesIdArray = this.recipes.map(function(elem) {return elem.id});
+  recipesIdArray = recipesIdArray.slice(0, 2);
+  //recipesIdArray = [325270];
+  async.everySeries(recipesIdArray, function(elem, callback) {
+    console.log(elem);
+    var queryURL = "recipes/" + elem;
+    $.ajax( {
+      url:queryURL,
+      method:"GET",
+      dataType: 'json'
+    })
+      .done(function(resp) {
+        //console.log(resp);
+
+        callback(null, true);
+      })
+      .fail(function(err){
+        callback(err, false);
+      })
+
+  }, function(err, result) {
+    //do nothing
+    console.log(err);
+  });
+  //console.log(resp);
 };
 
 RecipesUI.prototype.setUpTab = function() {
@@ -45,7 +79,8 @@ window.onload = function() {
   window.recipesUI = new RecipesUI();
 
   window.recipesUI.getAllRecipes()
-    .then(window.recipesUI.displayAllRecipes)
+    .then(window.recipesUI.displayAllRecipes.bind(window.recipesUI))
+    .then(window.recipesUI.setupRecipesModalCards.bind(window.recipesUI))
     .then(window.recipesUI.setUpTab);
 };
 
